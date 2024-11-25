1bedb80fb13278e3a00680e6ee3cfd1de5e096a0
timmy <sparynxcyberneti@gmail.com>
2024-11-25 21:39:17 +0100

    Add .gitignore

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+import Model from "./components/Model"
+import Features from "./components/Features"
+import Chip from "./components/Chip"
+import Footer from "./components/Footer"
+
+import * as Sentry from "@sentry/react"
+
+
+const App = () => {
+  // return <button onClick={() => {throw new Error("This is your first error!");}}>Break the world</button>;
+
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights/>
+      <Model />
+      <Features />
+      <Chip />
+      <Footer />
+    </main>
+  )
+}
+
+export default Sentry.withProfiler (App);
